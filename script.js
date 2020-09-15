6d226eb1eaea38f87d56ea0d80090130d00b5099
lroco <l.rocoski@gmail.com>
2020-09-15 15:39:25 -0400

    Updated js with attr and effects

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
+    $("#answer").hide();
     var magic8Ball = {};
-    magic8Ball.listOfAnswers = ["It is certain", "It is decidedly so, Without a doubt", "Yes – definitely", "Signs point to yes", "Reply hazy", "Better not tell you now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Indubitably", "In your dreams."];
+    magic8Ball.listOfAnswers = ["Yes – definitely", "Signs point to yes", "Reply hazy", "Better not tell you now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Indubitably", "In your dreams."];
+    
     magic8Ball.askQuestion = function(question){
         var randomNumber = Math.random();
         var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
         var randomIndex = Math.floor(randomNumberForListOfAnswers);
         var answer = this.listOfAnswers[randomIndex];
+        // $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
      
         $("#answer").text( answer );
      
@@ -13,7 +16,17 @@ $(document).ready(function(){
         console.log(answer);
     };
     var onClick = function() {
-        var question = prompt("ASK A YES/NO QUESTION!");magic8Ball.askQuestion(question);
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+        // $("#answer").hide();
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        $("#8ball").effect("shake");
+        setTimeout (
+            function() {
+                var question = prompt("Ask a yes or no question")
+                magic8Ball.askQuestion(question)
+            }, 500);
     };
-    $("#questionButton").click( onClick ); 
+    $("#questionButton").click( onClick );
+    $("#answer").fadeIn(4000); 
     });
